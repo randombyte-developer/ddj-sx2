6f6646e1e334385e54ec05ce740ad0726c619fa7
Sven Rahn <apps.randombyte@gmail.com>
2019-11-13 23:00:16 +0100

    Reorganized initialization

--- src/ddj-sx2.ts
+++ src/ddj-sx2.ts
@@ -2,8 +2,12 @@ import { Button } from "@controls/button";
 import { Deck } from "@/deck";
 import { FineMidiControl } from "@controls/fineMidiControl";
 import { log, toggleControl, makeLedConnection } from "@/utils";
+import { MidiControl } from "./controls/midiControl";
 
 const seratoHeartbeat = [0xF0, 0x00, 0x20, 0x7F, 0x50, 0x01, 0xF7];
+
+const decks = [1, 2, 3, 4].map(channel => new Deck(channel));
+let deckIndependentControls: MidiControl[];
 
 export function init(): void {
     // serato heartbeat to keep some additional functionality like jog wheel touch events
@@ -11,37 +15,30 @@ export function init(): void {
         midi.sendSysexMsg(seratoHeartbeat, seratoHeartbeat.length);
     }, false);
 
-    createControls();
-    registerConnectionCallbacks();
-}
+    deckIndependentControls = [
+        new FineMidiControl(0xB6, 0x1F, 0x3F, {
+            onValueChanged: value => {
+                engine.setParameter("[Master]", "crossfader", value);
+            }
+        }),
+        new Button(0x96, 0x63, {
+            onPressed: () => {
+                toggleControl("[Master]", "headSplit");
+            }
+        })
+    ];
 
-const decks = [1, 2, 3, 4].map(channel => new Deck(channel));
-
-const controls = [
-    new FineMidiControl(0xB6, 0x1F, 0x3F, {
-        onValueChanged: value => {
-            engine.setParameter("[Master]", "crossfader", value);
-        }
-    }),
-    new Button(0x96, 0x63, {
-        onPressed: () => {
-            toggleControl("[Master]", "headSplit");
-        }
-    })
-];
-
-function createControls() {
     // Effects
     for (const effectUnit of [1, 2]) {
         for (const effectNumber of [1, 2, 3]) {
             const group = `[EffectRack1_EffectUnit${effectUnit}_Effect${effectNumber}]`;
 
-            controls.push(new FineMidiControl(0xB3 + effectUnit, 0x00 + (effectNumber * 2), 0x20 + (effectNumber * 2), {
+            deckIndependentControls.push(new FineMidiControl(0xB3 + effectUnit, 0x00 + (effectNumber * 2), 0x20 + (effectNumber * 2), {
                 onValueChanged: value => {
                     engine.setParameter(group, "meta", value);
                 }
             }));
-            controls.push(new Button(0x93 + effectUnit, 0x46 + effectNumber, {
+            deckIndependentControls.push(new Button(0x93 + effectUnit, 0x46 + effectNumber, {
                 onPressed: () => {
                     toggleControl(group, "enabled");
                 }
@@ -50,9 +47,7 @@ function createControls() {
             makeLedConnection(group, "enabled", 0x93 + effectUnit, 0x46 + effectNumber);
         }
     }
-}
 
-function registerConnectionCallbacks() {
     makeLedConnection("[Master]", "headSplit", 0x96, 0x63);
 }
 
@@ -65,7 +60,7 @@ export function midiInput(channel: number, midiNo: number, value: number, status
         }
     }
 
-    for (const control of controls) {
+    for (const control of deckIndependentControls) {
         control.offerValue(status, midiNo, value);
     }
 }
